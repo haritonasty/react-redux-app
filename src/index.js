e019dd90cb44b601e2c35783d852ac61a8220989
Anastasya Kharitonova <a.kharitonovaspb@gmail.com>
2017-12-11 14:18:44 +0300

    added react-hot-loader & node-sass-chokidar

--- src/index.js
+++ src/index.js
@@ -2,7 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import {AppContainer} from 'react-hot-loader';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+const render = Component => {
+	ReactDOM.render(
+		<AppContainer>
+			<Component />
+		</AppContainer>,
+		document.getElementById('root'),
+	)
+};
+
+render(App);
+
+// Webpack Hot Module Replacement API
+if (module.hot) {
+	module.hot.accept('./App', () => { render(App) })
+}
